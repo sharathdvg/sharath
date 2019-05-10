6499213f960bb95042194d2cac52ecf0a5246daf
sharath kumara HK <50479450+sharathdvg@users.noreply.github.com>
2019-05-10 13:14:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabdaExpression
+{
+    delegate int Add(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add fun = delegate (int n1, int n2) { return n1 + n2; };
+            //Add fun = (int n1, int n2) => { return n1 + n2; };
+            //Add fun = (n1,n2) => {return n1 + n2; };
+            Add fun = (n1, n2) => n1 + n2;
+
+            Console.WriteLine(fun(10,20));
+        }
+    }
+}
